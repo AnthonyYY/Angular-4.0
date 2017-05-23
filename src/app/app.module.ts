c6b036e173eb6350a79555cdb812c42c052ff902
AnthonyYY <yjh2332@gmail.com>
2017-05-23 20:55:40 +0800

    [program initialization and basic component implementation]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
+import { StarsComponent } from './stars/stars.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    CarouselComponent,
+    FooterComponent,
+    StarsComponent,
+    NavbarComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
